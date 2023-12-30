c7c47e08615f00bbb5ba10d20f7857e616b2b059
Christine Teig <christine.teig@moller.no>
2023-12-30 11:09:27 +0100

    skeleton of api, including open api spec

---
+++ src/main/kotlin/com/otiming/otimingapi/OTimingApiApplication.kt
@@ -0,0 +1,11 @@
+package com.otiming.otimingapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OTimingApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<OTimingApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/otiming/otimingapi/OTimingApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.otiming.otimingapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OTimingApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
