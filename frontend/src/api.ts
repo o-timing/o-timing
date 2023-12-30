5a0e378bf1c9bfb869599ecf152b494525a5e1ab
Eirik Meland <eirik.meland@gmail.com>
2023-12-30 14:30:19 +0100

    openapi-fetch

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,19 +1,26 @@
+import createClient from "openapi-fetch";
+import {components, paths} from "./schema";
+
+type Participant = components["schemas"]["Participant"];
+
 const host = "localhost"
 const port = "3000"
 
-import {components} from "./schema";
-type Participant = components["schemas"]["Participant"];
-
 export const fetchAllParticipants: () => Promise<Participant[]> = async () => {
-    try {
-        const response = await fetch(`http://${host}:${port}/participants`);
-        const data = await response.json() as Participant[];
-        return data;
-    } catch (error) {
+    const {GET} = createClient<paths>({baseUrl: `http://${host}:${port}/`});
+
+    const {
+        data, // only present if 2XX response
+        error, // only present if 4XX or 5XX response
+    } = await GET("/v1/participants", );
+
+    if (error) {
         console.error(error);
-        return []
+        return [];
     }
+    return data;
 };
+
 
 export const searchLopere: (searchString: string) => Promise<Participant[]> = async (searchString: string) => {
     try {
